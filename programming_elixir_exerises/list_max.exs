194f90dc3abffb88df84c3a1ac9f943bbf3b1649
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2016-09-13 20:34:24 +0100

    Refactor after seeing answers

--- programming_elixir_exerises/list_max.exs
+++ programming_elixir_exerises/list_max.exs
@@ -2,10 +2,6 @@ defmodule MaxList do
   def max([n]), do: n
   def max([head | tail]), do: check_max(head, tail)
 
-  defp check_max(first, [head | tail]) when first >= head do
-    max([first | tail])
-  end
-  defp check_max(first, [head | tail]) when first <= head do
-    max([head | tail])
-  end
+  defp check_max(first, [head | tail]) when first >= head, do: max([first | tail])
+  defp check_max(_first, [head | tail]), do: max([head | tail])
 end
