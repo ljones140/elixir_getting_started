d41e2bf3046ca6c6cfa54665ee7d69a34b41ee37
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2016-09-18 20:01:50 +0100

    Refactored to make better

--- programming_elixir_exerises/lists_and_recursion/span.exs
+++ programming_elixir_exerises/lists_and_recursion/span.exs
@@ -1,7 +1,6 @@
 defmodule MyList do
-  def span(from, to), do: generate_list(from, to)
-  defp generate_list(from, to) when from > to, do: []
-  defp generate_list(head = from, to) do
-    [ head | generate_list(head + 1, to) ]
+  def span(from, to) when from > to, do: []
+  def span(head = from, to) do
+    [ head | span(head + 1, to) ]
   end
 end
