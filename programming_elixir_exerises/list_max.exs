1eae620945160e8b25bed58522a45d045b1810ca
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2016-09-18 18:41:49 +0100

    Fix max list_max

--- programming_elixir_exerises/list_max.exs
+++ programming_elixir_exerises/list_max.exs
@@ -1,6 +1,7 @@
 defmodule MaxList do
+  def max([]), do: nil
   def max([n]), do: n
-  def max([head | tail]), do: check_max(head, tail)
+  def max([head| tail]), do: check_max(head, tail)
 
   defp check_max(first, [head | tail]) when first >= head, do: max([first | tail])
   defp check_max(_first, [head | tail]), do: max([head | tail])
